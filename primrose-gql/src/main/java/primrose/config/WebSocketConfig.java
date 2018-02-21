2ecacd62fa2edbfffdf5a8fa7612af30889aed46
Rene Svetina <rene.svetina@gmail.com>
2018-02-21 15:08:58 +0100

    df

--- primrose-gql/src/main/java/primrose/config/WebSocketConfig.java
+++ primrose-gql/src/main/java/primrose/config/WebSocketConfig.java
@@ -1,14 +1,13 @@
 package primrose.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
+//@Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void configureMessageBroker(final MessageBrokerRegistry config) {
